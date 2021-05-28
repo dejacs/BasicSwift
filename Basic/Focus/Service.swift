32ec2af32ca9857269a2fff846f7b3779000b676
Jade Silveira <jadecs8@gmail.com>
2021-05-28 09:56:44 -0300

    add basic swift

---
+++ Basic/Focus/Service.swift
@@ -0,0 +1,62 @@
+//
+//  Service.swift
+//  Basic
+//
+//  Created by Jade Silveira on 27/05/21.
+//
+
+import Foundation
+
+class Service {
+    func usaFetch() {
+        fetchSomething {
+            print("Fazer algo após terminar a função fetchSomething")
+        }
+        
+        fetchOtherSomething { stringRetornada in
+            print(stringRetornada)
+        }
+        
+        fetchAnotherSomething(id: 2) { stringRetornada in
+            print(stringRetornada)
+        }
+    }
+    
+    func fetchSomething(completou: () -> Void) {
+        print("kjhvhjhj")
+        let islala = true
+        
+        completou()
+    }
+    
+    func fetchOtherSomething(completou: (String) -> Void) {
+        print("kjhvhjhj")
+        let islala = true
+        let umaVariavel = "Retorno de um serviço"
+        completou(umaVariavel)
+    }
+    
+    func fetchAnotherSomething(id: Int, completou: (String) -> Void) {
+        print("Posso fazer uma requisição passando esse id como parâmetro: \(id)")
+        let umaVariavel = "Retorno de um serviço"
+        completou(umaVariavel)
+    }
+    
+    func fetchSomethingElse(id: Int, completou: (Result<Contact, MeuErro>) -> Void) {
+        print("Posso fazer uma requisição passando esse id como parâmetro: \(id)")
+        
+        // Retorno sucesso passando um contato. Result é como um enum na forma de utilizar
+        completou(.success(Contact()))
+        
+        // Retorno um erro que eu criei (MeuErro) desde este que implemente Error
+        completou(.failure(.generic))
+    }
+}
+
+struct Contact: Codable {
+    
+}
+
+enum MeuErro: Error {
+    case generic
+}
---
+++ Basic/Focus/ViewController.swift
@@ -0,0 +1,178 @@
+//
+//  ViewController.swift
+//  Basic
+//
+//  Created by Jade Silveira on 27/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    typealias UmType = (String, Int)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Teste().olar()
+    }
+
+    /*
+     Tipagens: String, Int, Double, Bool, CGFloat
+     */
+    
+    let constante: String = "Uma constante"
+    var variavel = 8
+    var umArray = [String]()
+    var umOpcional: Bool? // pode ser nulo
+    
+    var umaTupla: (String, Int) = ("Oi", 1)
+    var outraTupla = (umParametro: "Olar", outroParametro: 2)
+    
+    func utilizacaoDeTupla() {
+        print(umaTupla.0)
+        print(outraTupla.outroParametro)
+    }
+    
+    func chamadaType() {
+        utilizacaoDeType(umParametro: ("habsjda", 3))
+    }
+    
+    func utilizacaoDeType(umParametro: UmType) {
+        print(umParametro.0)
+    }
+    
+    func umaFuncao(primeiroParametro: Int) -> Int {
+        primeiroParametro + 5
+    }
+    
+    func addSomething() {
+        umArray.append("Algum valor")
+    }
+    
+    func tratamentoDeOpcional() {
+        guard let variavelDesembrulhada = umOpcional else {
+            // se opcional for nulo vai cair nesse else
+            return
+        }
+        
+        print(variavelDesembrulhada)
+        
+        if let outraVariavelDesembrulhada = umOpcional {
+            print(outraVariavelDesembrulhada)
+        }
+    }
+    
+    enum UmEnum {
+        case umCaso
+    }
+    
+    enum OutroEnum {
+        case umCaso(umAlias: String)
+        case outroCaso(outroAlias: String)
+        case maisUmOutro(maisUmOutro: String)
+    }
+    
+    enum Error: String {
+        case parseError = "Parse Error"
+        case genericError = "Generic Error"
+        case connectionError = "Connection Error"
+    }
+    
+    enum Aba: Int {
+        case transaction
+        case pix
+    }
+    
+    enum CoisasDeLayout {
+        static let mensagemDeErro = "basbdkasdl"
+        static let umaImage = UIImage()
+    }
+    
+    func utilizacoesDeEnum() {
+        print(UmEnum.umCaso)
+        // Passando um alias
+        let enumComAlias = OutroEnum.outroCaso(outroAlias: "Olar")
+        
+        switch enumComAlias {
+        case .umCaso(let umAlias):
+            print(umAlias)
+        case let .outroCaso(outroAlias):
+            print(outroAlias)
+        case .maisUmOutro:
+            print("")
+        }
+        
+        let umErroDaAPI = Error.genericError.rawValue
+    }
+    
+    func teste(isAvailable: Bool) {
+        isAvailable ? print("Está disponível") : print("Está desabilitado")
+        print(isAvailable ? "Está disponível" : "Está desabilitado")
+        isAvailable ? print("Está disponível") : utilizacoesDeEnum()
+    }
+    
+    func tiposDeFor() {
+        let umaLista = [String]()
+        
+        for umItem in umaLista {
+            print(umItem)
+        }
+        
+        umaLista.forEach { item in
+            print(item)
+        }
+    }
+    
+    func operadores() {
+        let isAvailable = true
+        let isOpen = false
+        let isCaixa = true
+        
+        guard isAvailable, isOpen, isCaixa else {
+            return
+        }
+        
+        if isAvailable, isOpen, isCaixa {
+            
+        }
+    }
+    
+    func returnNaoAceita() -> Bool {
+        let isAvailable = true
+        let isOpen = false
+        let isCaixa = true
+        
+        return isAvailable && isOpen && isCaixa
+    }
+}
+
+class Abobora {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct Batata {
+    var name: String
+}
+
+class Teste {
+    func olar() {
+        let abobora = Abobora(name: "Abobora")
+        let batata = Batata(name: "Batata")
+        
+        var outraAbobora = abobora
+        var outraBatata = batata
+        
+        print(abobora.name + " ----- " + outraAbobora.name)
+        print(batata.name + " ----- " + outraBatata.name)
+        
+        outraAbobora.name = "Outra abobora"
+        outraBatata.name = "Outra batata"
+        
+        print(abobora.name + " ----- " + outraAbobora.name)
+        print(batata.name + " ----- " + outraBatata.name)
+    }
+}
